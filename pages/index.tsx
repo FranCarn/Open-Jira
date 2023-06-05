b159550a9897bc60f09d818052992ed676b1f9d6
@FranCarn <carnevalefranco99@gmail.com>
2023-06-05 14:38:15 -0300

    cards done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-import { Typography } from "@mui/material";
 import type { NextPage } from "next";
-import Head from "next/head";
+import { Card, CardHeader, Grid } from "@mui/material";
 import { Layout } from "../components/layout";
 
 const Home: NextPage = () => {
   return (
-    <Layout title="Home">
-      <Typography variant="h1" color="primary"></Typography>
+    <Layout title="Home - Open Jira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh-100px)" }}>
+            <CardHeader title="Pendientes" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh-100px)" }}>
+            <CardHeader title="En Progreso" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh-100px)" }}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
