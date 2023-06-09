65f921616627350e5d5874f314fcc7324e52fb6c
@FranCarn <carnevalefranco99@gmail.com>
2023-06-09 12:41:37 -0300

    middleware implemented

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith("/api/entries/")) {
+    const id = req.nextUrl.pathname.replace("/api/entries/", "");
+    if (!checkMongoIDRegExp.test(id)) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/api/bad-request";
+      url.search = `?message=${id} is not Valid ID`;
+      return NextResponse.rewrite(url);
+    }
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/api/entries/:path*",
+};
