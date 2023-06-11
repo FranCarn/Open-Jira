fe340f522a201a09286b359a31b38e5a3f192d46
@FranCarn <carnevalefranco99@gmail.com>
2023-06-11 14:44:54 -0300

    fix error in edit entry

--- database/dbEntries.ts
+++ database/dbEntries.ts
@@ -1,12 +1,13 @@
-import mongoose from "mongoose";
+import { isValidObjectId } from "mongoose";
 import { Entry, IEntry } from "../models";
-import { db } from ".";
+import { db } from "./";
 
 export const getEntryById = async (id: string): Promise<IEntry | null> => {
-  if (!mongoose.isValidObjectId(id)) return null;
+  if (!isValidObjectId(id)) return null;
 
   await db.connect();
   const entry = await Entry.findById(id).lean();
   await db.disconnect();
+
   return JSON.parse(JSON.stringify(entry));
 };
